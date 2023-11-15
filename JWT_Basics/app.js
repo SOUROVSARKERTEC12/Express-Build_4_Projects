d600adc95a1958daa0d134d160418e2b08a80d7b
SOUROV <sourovsarker005@gmail.com>
2023-11-15 16:36:33 +0600

    03 project end with lots of error handler

--- JWT_Basics/app.js
+++ JWT_Basics/app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const mainRouter = require('./routes/main')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const connectDB = require("./db/connect");
@@ -12,20 +13,22 @@ const connectDB = require("./db/connect");
 app.use(express.static('./public'));
 app.use(express.json());
 
+app.use('/api/v1', mainRouter)
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
-const welcome = ()=>{
-  console.log(`Server is listening on port ${port}...`)
+const welcome = () => {
+    console.log(`Server is listening on port ${port}...`)
 }
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI)
-    app.listen(port, welcome)
-  } catch (error) {
-    console.log(error)
-  }
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, welcome)
+    } catch (error) {
+        console.log(error)
+    }
 };
 start().then(r => console.log('server stating....'))
