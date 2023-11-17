b5bee89b62a9eb0c0c6923c836dfdb55690a1dde
SOUROV <sourovsarker005@gmail.com>
2023-11-17 20:01:25 +0600

    04 project end

--- Jobs_API/app.js
+++ Jobs_API/app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+
 const express = require('express');
 const app = express();
 
@@ -16,8 +24,19 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false
+}))
 app.use(express.json());
 // extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 
 // routes
 app.use('/api/v1/auth', authRouter)
